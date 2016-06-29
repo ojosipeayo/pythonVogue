8af64d4884f0997382acaa3a3c03db54cae0a935
ojosipeayo <ojosipeayo@gmail.com>
2016-06-29 16:50:27 +0100

    Update testpay.py

--- pythonVogue/demos/testpay.py
+++ pythonVogue/demos/testpay.py
@@ -1,12 +1,16 @@
-import transaction
+from pyvogue import Transaction
 
-trx = Transaction()
+
+s = Transaction()
+
 
 params = {'v_merchant_id':'demo',
           'memo':'testing',
           'total':'1200'
             }
 
-t = trx.paylink(params)
+t = s.paylink(params)
 
 print(t.text)
+
+#https://voguepay.com/pay/bnlink/1467199318-x0
