ef7c529fef515c0bc6e6a4bb7421f80cf21490bc
ojosipeayo <ojosipeayo@gmail.com>
2016-06-29 17:07:04 +0100

    Update testfetch.py

--- pythonVogue/demos/testfetch.py
+++ pythonVogue/demos/testfetch.py
@@ -1,1 +1,14 @@
 
+from pyvogue import Transaction
+
+
+s = Transaction()
+
+
+'''The first param is the transaction to be queried
+    the second param is the result format
+    the third param is the option to decode to python dictionary'''
+
+#r = s.getall('xxxxxxxxxx','json')
+r = s.getall('xxxxxxxx','json',True)
+print(r)
